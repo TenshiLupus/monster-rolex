c2d35e3fd48b2c6edd56c47be617005e5665ba97
Tenshilupus <Angel__CM@hotmail.com>
2023-02-08 14:17:49 +0100

    basic concepts

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
+import {Component} from 'react'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
+class App extends Component{
+
+  constructor (){
+    super() 
+
+    this.state = {
+      name: {firstName: 'Angel', lastName: 'Cardenas Martinez'},
+      company: 'practice'
+    }
+  }
+
+  render() {
+    return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
-  );
+    )
+  }
 }
 
 export default App;
