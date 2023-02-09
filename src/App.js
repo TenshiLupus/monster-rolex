e53ff150b148757dbffa25ae492ad54d53c39794
Tenshilupus <Angel__CM@hotmail.com>
2023-02-09 13:13:23 +0100

    Filtering items

--- src/App.js
+++ src/App.js
@@ -1,24 +1,76 @@
-import {Component} from 'react'
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
+import logo from "./logo.svg";
 
-class App extends Component{
+import CardList from './components/CardList/CardList.component' 
 
-  constructor (){
-    super() 
+import "./App.css";
+
+
+class App extends Component {
+  constructor() {
+    super();
 
     this.state = {
-      name: {firstName: 'Angel', lastName: 'Cardenas Martinez'},
-      company: 'practice'
-    }
+      monsters: [],
+      searchField: "",
+    };
   }
 
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  //This funciton gets initialized on the construction of the compoenent and does not necessarily reinitialize it on the change event. This OPTIMIZES the process
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
+  };
+
   render() {
+
+    //Destructuring this to shorten variable names and make it easier to read
+    const {monsters, searchField} = this.state;
+    const {onSearchChange} = this;
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    })
+
     return (
-    <div className="App">
-      
-    </div>
-    )
+      <div className="App">
+        <input
+          className="search-box"
+          type="search"
+          placeholder="search monsters"
+          onChange={this.onSearchChange}
+        />
+
+        {/* {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })} */}
+
+        <CardList monstersr={filteredMonsters} />
+
+        <button onClick />
+      </div>
+    );
   }
 }
 
---
+++ src/components/CardList/CardList.component.jsx
@@ -0,0 +1,21 @@
+import {Component} from 'react';
+
+
+class CardList extends Component {
+    render() {
+        return (
+            <div>
+                <h1>Hello im the cardList component</h1>
+                <div>
+                    <p>text</p>
+                </div>
+
+                <div></div>
+
+            </div>
+        )
+    }
+}
+
+export default CardList;
+
